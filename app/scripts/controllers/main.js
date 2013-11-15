ad85b02c421e137b5db45da02cb5f52c7d594032
Sami Pippuri <pippuri@gmail.com>
2013-11-15 12:32:23 +0200

    initial commit of the base files created with Yo for Angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('onlineSoapboxApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
